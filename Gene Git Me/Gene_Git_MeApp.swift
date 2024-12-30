a93e52ecb55fcc68ffa4628e921ad1fdfb4af97b
“thebariscc” <thebariscc@gmail.com>
2024-12-31 02:06:57 +0300

    Initial Commit

---
+++ Gene Git Me/Gene_Git_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gene_Git_MeApp.swift
+//  Gene Git Me
+//
+//  Created by Barış Can Ceylan on 31.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Gene_Git_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
